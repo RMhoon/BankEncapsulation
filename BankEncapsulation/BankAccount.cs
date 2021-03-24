6451ef67d3f06871465606da1b95a492ed46ee8e
RMhoon <r_mhoon@hotmail.com>
2021-03-23 20:45:27 -0400

    Adding code

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double value)
+        {
+            _balance += value;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankacct = new BankAccount();
+
+            Console.WriteLine("How much money are you depositing today?");
+            var deposit = double.Parse(Console.ReadLine());
+            bankacct.Deposit(deposit);
+
+            Console.WriteLine($"Thank you for your deposit.  Your balance is ${bankacct.GetBalance():#,##0}");
+        }
+    }
+}
